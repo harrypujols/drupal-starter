a6706f2550c7b9b180986c5e5837b785770fcce2
Harry Pujols <harrypujols@me.com>
2017-08-03 18:24:19 -0400

    retooling

--- Vagrantfile
+++ Vagrantfile
@@ -9,10 +9,7 @@ Dir.mkdir(PROJECT) unless File.exists?(PROJECT)
 Vagrant.configure('2') do |config|
   config.vm.box = 'harrypujols/sandbox'
   config.vm.network 'forwarded_port', guest: 80, host: PORT
-<<<<<<< HEAD
   config.vm.network 'forwarded_port', guest: 10000, host: 10000
-=======
->>>>>>> master
   config.vm.synced_folder './' + PROJECT, '/var/www/html', :mount_options => ['dmode=777','fmode=666']
   config.vm.provision :shell, path: 'provision.sh', args: PROJECT + " " + PORT
 end
--- provision.sh
+++ provision.sh
@@ -5,40 +5,15 @@ PASSWORD='root'
 PROJECT=$1
 PORT=$2
 
+# register packages
+add-apt-repository ppa:ondrej/php
+
 # update / upgrade
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+apt-get upgrade
 
-# install php latest
-apt-get install -y php libapache2
-
-# install composer
-apt-get install -y zip unzip composer
-
-# install mysql and give password to installer
-debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-apt-get -y install mysql-server php-mysql
-
-# install phpmyadmin and give password(s) to installer
-# for simplicity I'm using the same password for mysql and phpmyadmin
-debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-apt-get -y install phpmyadmin
-phpenmod mcrypt
-
-# setup mysql user
-MY=$(cat <<EOF
-[client]
-user=root
-password=root
-host=localhost
-EOF
-)
-echo "$MY" > /home/vagrant/.my.cnf
+# add php packages
+apt-get install -y php-uploadprogress
 
 # install drush
 apt-get install -y drush
@@ -51,18 +26,6 @@ chmod +x /usr/local/bin/drupal
 # create a database
 mysql --user=$PASSWORD --password=$PASSWORD -e "create database $PROJECT;"
 
-# install apache
-apt-get install -y apache2
-
-# enable mods
-a2enmod rewrite
-a2enmod headers
-a2enmod expires
-a2enmod include
-
-# change apache configurations
-sed -i "/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/" /etc/apache2/apache2.conf
-
 # install drupal if not present
 if [ ! -f /var/www/html/index.php  ]; then
   rm /var/www/html/index.html
@@ -74,25 +37,8 @@ fi
 # symlink site's folder
 ln -s /var/www/html /home/vagrant/$PROJECT
 
-# setup hosts file
-VHOST=$(cat <<EOF
-<VirtualHost *:80>
-    DocumentRoot "/var/www/$PROJECT"
-    <Directory "/var/www/$PROJECT">
-        AllowOverride All
-        Require all granted
-    </Directory>
-</VirtualHost>
-EOF
-)
-
-echo "$VHOST" > /etc/apache2/sites-available/default.conf
-
 # restart apache
 service apache2 restart
-
-# install git
-apt-get -y install git
 
 # all done
 echo "database \"$PROJECT\" created"
