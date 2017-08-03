2f165025c465d494df8f1696e255c5f71a044855
Harry Pujols <harrypujols@me.com>
2017-08-03 14:54:20 -0400

    Cleaner provision

--- Vagrantfile
+++ Vagrantfile
@@ -1,12 +1,14 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-PROJECT='public'
+PROJECT='drupal'
 PORT='8080'
+
+Dir.mkdir(PROJECT) unless File.exists?(PROJECT)
 
 Vagrant.configure('2') do |config|
   config.vm.box = 'bento/ubuntu-16.04'
   config.vm.network 'forwarded_port', guest: 80, host: PORT
-  config.vm.synced_folder './', '/var/www/', :mount_options => ['dmode=777','fmode=666']
+  config.vm.synced_folder './' + PROJECT, '/var/www/html', :mount_options => ['dmode=777','fmode=666']
   config.vm.provision :shell, path: 'provision.sh', args: PROJECT + " " + PORT
 end
--- provision.sh
+++ provision.sh
@@ -6,32 +6,29 @@ PROJECT=$1
 PORT=$2
 
 # update / upgrade
-sudo apt-get update
+apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
-# symlink site's folder
-sudo ln -s /var/www/html /home/vagrant/$PROJECT
-
 # install php latest
-sudo apt-get install -y php libapache2
+apt-get install -y php libapache2
 
 # install composer
-sudo apt-get install -y zip unzip composer
+apt-get install -y zip unzip composer
 
 # install mysql and give password to installer
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server php-mysql
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+apt-get -y install mysql-server php-mysql
 
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
-sudo phpenmod mcrypt
+debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+apt-get -y install phpmyadmin
+phpenmod mcrypt
 
 # setup mysql user
 MY=$(cat <<EOF
@@ -44,29 +41,38 @@ EOF
 echo "$MY" > /home/vagrant/.my.cnf
 
 # install drush
-sudo apt-get install -y drush
+apt-get install -y drush
 
 # install drupal console
 curl https://drupalconsole.com/installer -L -o drupal.phar
-sudo mv drupal.phar /usr/local/bin/drupal
-sudo chmod +x /usr/local/bin/drupal
+mv drupal.phar /usr/local/bin/drupal
+chmod +x /usr/local/bin/drupal
 
 # create a database
 mysql --user=$PASSWORD --password=$PASSWORD -e "create database $PROJECT;"
 
-# # install drupal if not present
-if [ ! "$( ls -A /var/www/$PROJECT )" ]; then
-  drush dl drupal-8 --destination=/var/www/ --drupal-project-rename=$PROJECT
-fi
-
 # install apache
-sudo apt-get install -y apache2
+apt-get install -y apache2
 
 # enable mods
-sudo a2enmod rewrite
-sudo a2enmod headers
-sudo a2enmod expires
-sudo a2enmod include
+a2enmod rewrite
+a2enmod headers
+a2enmod expires
+a2enmod include
+
+# change apache configurations
+sed -i "/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/" /etc/apache2/apache2.conf
+
+# install drupal if not present
+if [ ! -f /var/www/html/index.php  ]; then
+  rm /var/www/html/index.html
+  drush dl drupal-8 --destination=/var/www/ --drupal-project-rename=$PROJECT
+  rsync -vau --delete-after /var/www/$PROJECT/ /var/www/html/
+  rm -rf /var/www/$PROJECT
+fi
+
+# symlink site's folder
+ln -s /var/www/html /home/vagrant/$PROJECT
 
 # setup hosts file
 VHOST=$(cat <<EOF
@@ -80,22 +86,13 @@ VHOST=$(cat <<EOF
 EOF
 )
 
-echo "$VHOST" > /etc/apache2/sites-available/$PROJECT.conf
-cp /etc/apache2/sites-available/$PROJECT.conf /etc/apache2/sites-available/default.conf
-sed -i "s/html/$PROJECT/g" /etc/apache2/sites-available/000-default.conf
-sed -i "s/\\/$PROJECT//g" /etc/apache2/sites-available/default.conf
-
-# clean up
-sudo rm -rf /var/www/html
-
-# change apache configurations
-sudo sed -i "/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/" /etc/apache2/apache2.conf
+echo "$VHOST" > /etc/apache2/sites-available/default.conf
 
 # restart apache
-sudo service apache2 restart
+service apache2 restart
 
 # install git
-sudo apt-get -y install git
+apt-get -y install git
 
 # all done
 echo "database \"$PROJECT\" created"
